25045d1ccd5ad166f42b485db5b55dfd8693560d
POGsss <frankiepinat14@gmail.com>
2025-03-10 15:23:34 +0800

    Lesson 10

--- public/script.js
+++ public/script.js
@@ -5,3 +5,8 @@ const type = document.querySelector("#type");
 const toFrom = document.querySelector("#toFrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+// Setting Form Submit Event
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
--- src/script.ts
+++ src/script.ts
@@ -5,3 +5,10 @@ const type = document.querySelector("#type") as HTMLInputElement;
 const toFrom = document.querySelector("#toFrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
+
+// Setting Form Submit Event
+form.addEventListener("submit", (e: Event) => {
+    e.preventDefault();
+
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
