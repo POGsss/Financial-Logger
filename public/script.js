283d62e16f8583c04f6cdf85851b0c577621034b
POGsss <frankiepinat14@gmail.com>
2025-03-22 16:55:02 +0800

    Designed the overall look and layout of the website

--- public/script.js
+++ public/script.js
@@ -1,2 +1,11 @@
 "use strict";
-// Getting Elements From Document
+// Getting Nav Elements
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+// Adding Event Listener To Nav Elements
+hamburger.addEventListener('click', function () {
+    navLinks.classList.toggle("active");
+});
+navLinks.addEventListener('click', function () {
+    navLinks.classList.toggle("active");
+});
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,11 @@
-// Getting Elements From Document
+// Getting Nav Elements
+const hamburger = document.getElementById("hamburger") as HTMLElement;
+const navLinks = document.getElementById("navLinks") as HTMLElement;
+
+// Adding Event Listener To Nav Elements
+hamburger.addEventListener('click', function() {
+    navLinks.classList.toggle("active");
+});
+navLinks.addEventListener('click', function() {
+    navLinks.classList.toggle("active");
+});
